b2f2c5617a418b33ac146422f96e6573595f6ec4
lbernstone <lbernstone@gmail.com>
2021-09-30 06:07:56 -0700

    fix error message
    Fixed error message, which was showing duty instead of channel.

--- src/Tone32.cpp
+++ src/Tone32.cpp
@@ -3,7 +3,7 @@
 void tone(uint8_t pin, unsigned int frequency, unsigned long duration, uint8_t channel)
 {
     if (ledcRead(channel)) {
-        log_e("Tone channel %d is already in use", ledcRead(channel));
+        log_e("Tone channel %d is already in use", channel);
         return;
     }
     ledcAttachPin(pin, channel);
